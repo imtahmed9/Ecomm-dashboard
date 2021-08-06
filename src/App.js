1fcd67f65608a249f53f2e160c0a7b21631f3173
imtiyaz ahmed <imtahmed9@gmail.com>
2021-08-06 17:47:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Ecomm Dashboard</h1>
+        <button>Normal Button</button>
+        <Button>Bootstarp Button</Button>
+    </div>
+  );
+
+}
+
+export default App;
